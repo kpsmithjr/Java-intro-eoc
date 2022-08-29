62d305624996ee1e251d08222b08ecee9ec863f5
France P <francepointdujour@gmail.com>
2022-08-29 15:23:01 -0400

    August 29th java test

--- src/main/java/com/car/App.java
+++ src/main/java/com/car/App.java
@@ -1,13 +1,23 @@
 package com.car;
 
+import javax.swing.text.Caret;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!");
+    
+
+    Car honda = new Car 
+    Car lexus = new Car
+     Car toyota = new Car("blue");
+            System.out.println()
+
     }
+    
 }
---
+++ src/main/java/com/car/Toyota.java
@@ -0,0 +1,11 @@
+package com.car;
+
+public class Toyota extends Car {
+  private boolean greatGasMileage;  
+
+
+public Toyota(String color, int year, int maxSpeed) {
+    super(color, year, maxSpeed);
+}
+
+}
