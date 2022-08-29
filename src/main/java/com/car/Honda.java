264d7c83a24051aa14feef26331530acb5a2c263
Lafacesus <jamaicanflower30000@gmail.com>
2022-08-29 15:04:51 -0700

    Final

---
+++ src/main/java/com/car/Honda.java
@@ -0,0 +1,14 @@
+package com.car;
+
+public class Honda {
+    
+
+Public static void main String([] arges) {
+
+}
+}
+System.out.print("String with the name color
+int with the name year
+int with the name speed (initial value 0)
+int with the name maxSpeed
+boolean with the name isElectric");
---
+++ src/main/java/com/car/Lexus.java
@@ -0,0 +1,12 @@
+package com.car;
+
+public class Lexus {
+    
+}
+public static void main( String[] args )
+boolean hasLeatherSeats
+
+hasLeatherSeats =true;
+{
+
+}
