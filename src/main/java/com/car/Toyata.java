ca5a718f501ef913da2bc02b36bf91b071e75c5b
omarfaruk <farukmd2004@gmail.com>
2022-08-30 10:43:13 -0500

    third commit

--- src/main/java/com/car/Toyata.java
+++ src/main/java/com/car/Toyata.java
@@ -1,5 +1,18 @@
 package com.car;
 
-public class Toyata {
+public class Toyata extends Car {
+    private boolean greatGasMileage;
+
+    public Toyata(String color, int year, int maxSpeed, boolean greatGasMileage){
+        super(color, year, maxSpeed);
+        this.greatGasMileage = greatGasMileage;
+    }
+    private boolean getGreatGasMileage(){
+        return greatGasMileage;
+    }
+
+    public void setGreatGasMileage(boolean getGreatGasMileage){
+        this.greatGasMileage = greatGasMileage;
+    }
     
 }
